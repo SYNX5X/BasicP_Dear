4edfcbfed230144bd66668a6f42db14653b8c218
Sutthawee_Dear <SYNX5X@users.noreply.github.com>
2025-07-23 20:41:52 +0700

    Final class03_Workshop

--- class03_Workshop.py
+++ class03_Workshop.py
@@ -13,26 +13,44 @@ while start :
         k = int(input("คุณจะจัดการมอนเตอร์นี้กี่รอบ"))
         for i in range(k):
             print("กรุณาเลือกอาวุธที่ใช้ในการโจมตี")
-            print("weapon1 = 50","กดเลข 1")
-            print("weapon2 = 30","กดเลข 2")
-            print("weapon3 = 100","กดเลข 3")
-            print("monsterhp = ",monster)
-            print("จำนวนครั้งที่เหลือ ",k - i )
-        w = int(input("เลือกอาวุธเอาชิ้นไหน"))
-        print("คุณเลือกอาวุธ", w)
-        if (w == 1) :
-            w1 = 200-50
-            print(w1)
-        
-       
-
-
-    
-
-
-
-
-
-    else :
-        print("กลับบ้านดีกว่า")
+            print(weapon1 )
+            print(weapon2)
+            print(weapon3)
+            print("Hp monster = ",monster)
+            print("เหลือโอกาศตีอยู่:",k - i)
+            w = int(input("เลือกอาวุธเอาชิ้นไหน"))
+            print("คุณเลือกอาวุธ", w)
+            if (w == 1) :
+                print("คุณตีไป 50 หน่วย")
+                monster -= weapon1
+                print("จำนวนเหลืดที่เหลือ",monster, "Hp")
+                if (monster < 0):
+                    print("ตีเเรกเกินนไปเว้ยย เเม่งมีสกิลเด้งเลือด 20 หน่วย")
+                    monster = 20
+            elif w == 2 :
+                print("คุณตีไป 30 หน่วย")
+                monster -= weapon2
+                print("จำนวนเหลืดที่เหลือ",monster, "Hp")
+                if (monster < 0):
+                    print("ตีเเรกเกินนไปเว้ยย เเม่งมีสกิลเด้งเลือด 20 หน่วย")
+                    monster = 20
+            elif w == 3 :
+                print("คุณตีไป 100 หน่วย")
+                monster -= weapon3
+                print("จำนวนเหลืดที่เหลือ",monster, "Hp")
+                if (monster < 0):
+                    print("ตีเเรกเกินนไปเว้ยย เเม่งมีสกิลเด้งเลือด 20 หน่วย")
+                    monster = 20
+            elif monster == 0 :
+                print("monster is dead hp 0")
+                break
+        if (monster > 0):
+            print("หมดโอกาศแล้ว")
+            print("mon ไม่ตาย = มึงตาย")
+            start = False
+        else:
+            print("มอนตายเครียเกมได้เเเล้วกลับบ้านได้")
+            start = False
+    else:
+        print("กลับบ้านนอนปลอดภัย")
         break
